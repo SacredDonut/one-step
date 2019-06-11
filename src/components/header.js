0b29cf8dbf129427fb86250209613b6a2ad32f7d
Jose-Antonio Rubio <jrubio@nexient.com>
2019-06-11 16:32:31 -0400

    First Commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,56 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import logo from '../images/logo.jpg'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const NavLink = props => <Link getProps={isActive} {...props} />
+
+const isActive = ({ isCurrent }) => {
+  return {
+    className: isCurrent ? 'active' : 'navlink'
+  }
+}
+
+const Header = ({ siteTitle }) => {
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `#072044`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+
+        {/* Title/Logo */}
+        <span style={{ display: 'flex', alignItems: 'center' }}>
+          <img 
+            src={logo} 
+            alt="OneStepLogo" 
+            style={{ 
+              borderRadius: '100%',
+              border: '6px solid #8EC8BF',
+              margin: '0 5px',
+              width: '50px' 
+            }}/>
+          <h1 style={{ margin: 0 }}>
+            <NavLink to="/">ne Step</NavLink>
+          </h1>
+        </span>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        
+      </div>
+    </header>
+)}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/layout.js
+++ src/components/layout.js
@@ -12,17 +12,21 @@ import { StaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 
+const GET_SITEMETADATA = graphql`
+  query{
+    site {
+      siteMetadata {
+        title,
+        author,
+        createdAt
+      }
+    }
+  }
+`
+
 const Layout = ({ children }) => (
   <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
+    query={GET_SITEMETADATA}
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
@@ -36,9 +40,15 @@ const Layout = ({ children }) => (
         >
           <main>{children}</main>
           <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+           <div
+             style={{
+               display: 'flex',
+               alignItems: 'center',
+               justifyContent: 'space-between'
+             }}
+           >
+             Built by {data.site.siteMetadata.author}, Created {data.site.siteMetadata.createdAt}
+          </div>
           </footer>
         </div>
       </>
